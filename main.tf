f07a5a35dfa7a4ea4e7e9577ee78e163380474e1
Sem Onyalo <sem.onyalo@slalom.com>
2022-01-11 23:37:31 -0500

    initial terraform files

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "gan_training_resource_group" {
+  name     = "gan_training_resource_group"
+  location = "westus2"
+}
